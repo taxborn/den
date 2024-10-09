7a5a1b59f8581350b140fb561a4f42055e3170a8
Braxton Fair <hello@taxborn.com>
2024-10-09 13:19:35 -0500

    feat: start cargo project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Den
+My website, written in Rust. I've tried a few different frameworks - [Astro](https://astro.build) being my favorite -
+but I've always wondered what it would be like to write my own website generator.
+
+## Requirements
+- Fast to compile, easy deployments
+- Use GitHub CI to deploy
+- CSP security, passes Google Lighthouse checks
+- Parses Markdown with frontmatter
---
+++ src/main.rs
@@ -0,0 +1,76 @@
+use std::net::SocketAddr;
+
+use bytes::Bytes;
+use http_body_util::{combinators::BoxBody, BodyExt, Empty, Full};
+use hyper::body::Frame;
+use hyper::server::conn::http2;
+use hyper::service::service_fn;
+use hyper::{body::Body, Method, Request, Response, StatusCode};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
+
+#[derive(Clone)]
+// An Executor that uses the tokio runtime.
+pub struct TokioExecutor;
+
+// Implement the `hyper::rt::Executor` trait for `TokioExecutor` so that it can be used to spawn
+// tasks in the hyper runtime.
+// An Executor allows us to manage execution of tasks which can help us improve the efficiency and
+// scalability of the server.
+impl<F> hyper::rt::Executor<F> for TokioExecutor
+where
+    F: std::future::Future + Send + 'static,
+    F::Output: Send + 'static,
+{
+    fn execute(&self, fut: F) {
+        tokio::task::spawn(fut);
+    }
+}
+
+async fn route(
+    req: Request<hyper::body::Incoming>,
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => Ok(Response::new(full("Try POSTing data to /echo such as: `curl localhost:3000/echo -XPOST -d \"hello world\"`"))),
+            // Simply echo the body back to the client.
+        (&Method::POST, "/echo") => Ok(Response::new(req.into_body().boxed())),
+        _ =>  {
+            let mut not_found = Response::new(empty());
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
+}
+
+fn empty() -> BoxBody<Bytes, hyper::Error> {
+    Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
+}
+
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = TcpListener::bind(addr).await?;
+    println!("Listening on http://{addr}");
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let io = TokioIo::new(stream);
+
+        tokio::task::spawn(async move {
+            if let Err(err) = http2::Builder::new(TokioExecutor)
+                .serve_connection(io, service_fn(route))
+                .await
+            {
+                println!("Error serving connection: {err:?}");
+            }
+        });
+    }
+}
